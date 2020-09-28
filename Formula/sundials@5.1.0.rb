22cee465a97b14909d9a153cdfb02cf24e7cd8fb
Valentin Sulzer <valentinsulzer@hotmail.com>
2020-09-28 10:57:40 -0400

    Add Sundials 5.1.0 formula

---
+++ Formula/sundials@5.1.0.rb
@@ -0,0 +1,44 @@
+class SundialsAT510 < Formula
+  desc "Nonlinear and differential/algebraic equations solver"
+  homepage "https://computation.llnl.gov/casc/sundials/main.html"
+  url "https://computation.llnl.gov/projects/sundials/download/sundials-5.1.0.tar.gz"
+  sha256 "fb22d14fad42203809dc46d046b001149ec4e901b23882bd4a80619157fd9b21"
+  revision 2
+
+  depends_on "cmake" => :build
+  depends_on "gcc" # for gfortran
+  depends_on "open-mpi"
+  depends_on "openblas"
+  depends_on "suite-sparse"
+
+  uses_from_macos "libpcap"
+  uses_from_macos "m4"
+
+  def install
+    blas = "-L#{Formula["openblas"].opt_lib} -lopenblas"
+    args = std_cmake_args + %W[
+      -DCMAKE_C_COMPILER=#{ENV["CC"]}
+      -DBUILD_SHARED_LIBS=ON
+      -DKLU_ENABLE=ON
+      -DKLU_LIBRARY_DIR=#{Formula["suite-sparse"].opt_lib}
+      -DKLU_INCLUDE_DIR=#{Formula["suite-sparse"].opt_include}
+      -DLAPACK_ENABLE=ON
+      -DBLA_VENDOR=OpenBLAS
+      -DLAPACK_LIBRARIES=#{blas};#{blas}
+      -DMPI_ENABLE=ON
+    ]
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+    end
+  end
+
+  test do
+    cp Dir[prefix/"examples/nvector/serial/*"], testpath
+    system ENV.cc, "-I#{include}", "test_nvector.c", "sundials_nvector.c",
+                   "test_nvector_serial.c", "-L#{lib}", "-lsundials_nvecserial"
+    assert_match "SUCCESS: NVector module passed all tests",
+                 shell_output("./a.out 42 0")
+  end
+end
